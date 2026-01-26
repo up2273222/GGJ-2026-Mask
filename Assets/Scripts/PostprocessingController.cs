efdb1bafbfe3c26a6378a3940542f6d92768e643
up2273222 <up2273222@myport.ac.uk>
2026-01-26 11:45:29 +0000

    Create shader things

---
+++ Assets/Scripts/PostprocessingController.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+[ExecuteInEditMode, ImageEffectAllowedInSceneView]
+public class PostprocessingController : MonoBehaviour
+{
+ public Shader postProcessShader;
+ private Material postProcessMaterial;
+ 
+ void OnRenderImage(RenderTexture src, RenderTexture dest)
+ {
+     if (postProcessMaterial == null)
+     {
+         postProcessMaterial = new Material(postProcessShader);
+     }
+ 
+ 
+   RenderTexture renderTexture = RenderTexture.GetTemporary(
+   src.width, //Width
+   src.height,  //Height
+   0,  //Depth buffer
+   src.format  //Format
+   );
+   
+   Graphics.Blit(src, renderTexture, postProcessMaterial);
+   Graphics.Blit(renderTexture, dest);
+   
+   RenderTexture.ReleaseTemporary(renderTexture);
+ }
+ 
+}
