8616365ac7638635edb16e329062d819a845e5de
Epi0616 <samuel.fyfield@outlook.com>
2026-01-26 17:09:34 +0000

    Player Controller Finished

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,43 +3,91 @@ using System.Collections.Generic;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed;
     [SerializeField] private Transform orientation;
-
+    
+    [SerializeField] private float moveSpeed;
     private float inputX;
     private float inputY;
-    
     private Vector3 moveDirection;
+    [SerializeField] private float dragAmount;
 
+    [SerializeField] private float jumpForce;
+    private float jumpCooldown;
+    private float jumpTimer;
+    
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private bool isGrounded;
+    
     private Rigidbody rb;
+    private CapsuleCollider capsuleCollider;
 
     void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        capsuleCollider = GetComponentInChildren<CapsuleCollider>();
         rb.freezeRotation = true;
+        rb.linearDamping = dragAmount;
     }
 
+    void Update()
+    {
+        Inputs();
+        GroundCheck();
+    }
+    
+    void FixedUpdate()
+    {
+        jumpTimer -= Time.deltaTime;
+        MovePlayer();
+        if (isGrounded && Input.GetKey(KeyCode.Space) && jumpTimer <= 0)
+        {
+            jumpTimer = jumpCooldown;
+            Jump();
+        }
+    }
     private void Inputs()
     {
-        inputX = Input.GetAxis("Horizontal");
-        inputY = Input.GetAxis("Vertical");
+        inputX = Input.GetAxisRaw("Horizontal");
+        inputY = Input.GetAxisRaw("Vertical");
     }
 
     private void MovePlayer()
     {
         moveDirection = orientation.forward * inputY + orientation.right * inputX;
         
-        rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+        rb.AddForce(moveDirection.normalized * (moveSpeed * 10f), ForceMode.Force);
+        SpeedControl();
     }
 
-    void Update()
+    private void SpeedControl()
     {
-        Inputs();
+        Vector3 flatVelocity = new Vector3(rb.linearVelocity.x, 0 , rb.linearVelocity.z);
+        if (flatVelocity.magnitude > moveSpeed)
+        {
+            Vector3 newVelocity = flatVelocity.normalized * moveSpeed;
+            rb.linearVelocity = new Vector3(newVelocity.x, rb.linearVelocity.y, newVelocity.z);
+        }
     }
-    
-    
-    void FixedUpdate()
+
+    private void GroundCheck()
     {
-        MovePlayer();
+        float sphereCastRadius = capsuleCollider.radius - 0.2f;
+        Vector3 sphereCastOrigin = groundCheck.position;
+        isGrounded = Physics.SphereCast(sphereCastOrigin, sphereCastRadius, Vector3.down, out RaycastHit hit, 1f);
+        
+        if (isGrounded)
+        {
+            rb.linearDamping = dragAmount;
+        }
+        else
+        {
+            rb.linearDamping = 0;
+        }
+    }
+
+    private void Jump()
+    {
+        rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0 , rb.linearVelocity.z);
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
     }
 }
