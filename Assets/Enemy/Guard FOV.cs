0ff8a57a5e76a0144d5004cf54e76340d300753b
BadPlays <Haydanfrench@hotmail.com>
2026-01-27 18:58:29 +0000

    Player Detection Added

---
+++ Assets/Enemy/Guard FOV.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class GuardFOV : MonoBehaviour
+{
+    public float Radius;
+    [RangeAttribute(0, 360)]
+    public float Angle;
+
+    public GameObject PlayerReference;
+
+    public LayerMask TargetMask;
+    public LayerMask ObstructionMask;
+
+    public bool PlayerDetected;
+
+    private void Start()
+    {
+        PlayerReference = GameObject.FindGameObjectWithTag("Player");
+        StartCoroutine(GuardFOVRoutine());
+    }
+
+
+    private IEnumerator GuardFOVRoutine()
+    {
+        Debug.Log("GuardRoutineStarted");
+        WaitForSeconds wait = new WaitForSeconds(0.2f);    
+
+        while (true)
+        {
+            yield return wait;
+            FieldOfViewCheck();
+        }
+    }
+
+    private void FieldOfViewCheck()
+    {
+        Collider[] rangeChecks = Physics.OverlapSphere(transform.position, Radius, TargetMask);
+
+        if (rangeChecks.Length != 0)
+        {
+            Transform target = rangeChecks[0].transform;
+            Vector3 directionToTarget = (target.position - transform.position).normalized;
+
+            if (Vector3.Angle(transform.forward, directionToTarget) < Angle / 2)
+            {
+                float distanceToTarget = Vector3.Distance(transform.position, target.position);
+
+                if(!Physics.Raycast(transform.forward, directionToTarget, distanceToTarget, ObstructionMask))
+                {
+                    PlayerDetected = true;
+                }
+                else
+                {
+                    PlayerDetected = false;
+                }
+            }
+            else
+            {
+                PlayerDetected = false;
+            }
+        }
+        else if(PlayerDetected)
+        {
+            PlayerDetected = false;
+        }
+    }
+}
---
+++ Assets/Enemy/GuardFOVVisualisation.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEditor;
+using System.Runtime.InteropServices;
+using UnityEngine.UIElements;
+
+[CustomEditor(typeof(GuardFOV))]
+public class GuardFOVVisualisation : Editor
+{
+
+    private void OnSceneGUI()
+    {
+        GuardFOV fov = (GuardFOV)target;
+        Handles.color = Color.white;
+        Handles.DrawWireArc(fov.transform.position, Vector3.up, Vector3.forward, 360, fov.Radius);
+
+        Vector3 viewAngle1 = DirectionFromAngle(fov.transform.eulerAngles.y, -fov.Angle / 2);
+        Vector3 viewAngle2 = DirectionFromAngle(fov.transform.eulerAngles.y, fov.Angle / 2);
+
+        Handles.color = Color.red;
+        Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngle1 * fov.Radius);
+        Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngle2 * fov.Radius);
+
+        if(fov.PlayerDetected)
+        {
+            Handles.color = Color.green;
+            Handles.DrawLine(fov.transform.position, fov.PlayerReference.transform.position);
+        }
+    }
+
+    private Vector3 DirectionFromAngle(float eulerY, float angleInDegrees)
+    {
+        angleInDegrees += eulerY;
+
+        return new Vector3(Mathf.Sign(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+    }
+}
